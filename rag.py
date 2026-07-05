8f49c84731c5dca5e53e7643e4bd26059ab9aaee
shahisking <shahiskhan26@gmail.com>
2026-07-05 16:04:56 +0530

    first commit

---
+++ rag.py
@@ -0,0 +1,57 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+loader = TextLoader("RAG.txt", encoding="utf-8")
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=50)
+chunks = splitter.split_documents(docs)
+len(chunks)
+
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+db = FAISS.from_documents(chunks, embeddings)
+retriever = db.as_retriever()
+
+from transformers import pipeline
+from langchain_community.llms import HuggingFacePipeline
+
+pipe = pipeline(
+    "text-generation",
+    model="Qwen/Qwen2.5-1.5B-Instruct",
+    max_new_tokens=256,
+    temperature=0.2
+)
+
+llm = HuggingFacePipeline(pipeline=pipe)
+ques=input("Ask your question about RAG: ")
+def rag_chain(question):
+    # 1. Retrieve similar docs
+    docs = retriever.invoke(question)
+
+    # 2. Combine the doc text
+    context = "\n\n".join([d.page_content for d in docs])
+
+    # 3. Format the prompt
+    prompt_text = f"""
+You are an AI assistant. Use ONLY the provided context to answer.
+If the answer is not in the context, say "I don't know".
+
+Context:
+{context}
+
+Question: {question}
+
+Answer:
+"""
+
+    # 4. Call the model
+    raw = llm.invoke(prompt_text)
+    answer = raw.replace(prompt_text, "").strip()
+    return answer
+
+response = rag_chain(ques)
+print(response)
